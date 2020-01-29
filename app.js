37b208be6790fe2de4fe6255195220f8fdcb372a
李坤鹏 <likunpeng@likunpengdeMacBook-Pro.local>
2020-01-29 14:26:34 +0800

    护 眼 模 式

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 $(document).ready(function(){
-    const imageG = {eye : chrome.runtime.getURL("eyemode.png")};
     //添加护眼模式按钮
-    $(".readerControls").prepend("<a id=\"eyemode\" title=\"护眼模式\" class=\"readerControls_item catalog\"><img id=\"eyeicon\" src=\"${imageG.eyemode}\" alt=\"护眼模式\"></img></a>");
-    $("#eyeicon").attr("src", imageG.eye);
+    $(".readerControls").prepend("<a id=\"eyemode\" title=\"护眼模式\" class=\"readerControls_item eyemode\"><span class=\"icon\"></span></a>");
+
+    $(".eyemode").click(function () {
+        $("body").toggleClass("eyemode");
+    });
 });
